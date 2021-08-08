fb106c3e278210ea59ff1573dda3f456376d179d
I_Choose_Bear_Paws <73148587+IChooseBearPaws@users.noreply.github.com>
2021-08-08 23:04:12 +0800

    mesh

---
+++ src/Mesh.cpp
@@ -0,0 +1,13 @@
+#include "Mesh.h"
+
+Mesh::Mesh(vector<Vertex> vertices_, vector<int> idx, vector<Texture> textures_)
+{
+}
+
+void Mesh::Draw(Shader myShader)
+{
+}
+
+void Mesh::setupMesh()
+{
+}
---
+++ src/Mesh.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <string>
+#include <vector>
+
+#include "Shader.h"
+
+using namespace std;
+using namespace glm;
+
+struct Vertex {
+	vec3 positoin;
+	vec3 normal;
+	vec2 texCoords;
+};
+
+struct Texture {
+	vec3 id;
+	string type;
+	string path;
+};
+
+class Mesh
+{
+public:
+	vector<Vertex> vertices;
+	vector<unsigned int> idxes;
+	vector<Texture> textures;
+	
+	Mesh(vector<Vertex> vertices_, vector<int> idx, vector<Texture> textures_);
+	void Draw(Shader myShader);
+
+private:
+	unsigned int VAO, VBO, EBO;
+	void setupMesh();
+};
+
